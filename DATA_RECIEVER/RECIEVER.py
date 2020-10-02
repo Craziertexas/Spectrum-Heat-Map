14f89ef115ed1c05d550713ed36e22a9ea08ff05
David Ruiz <druizeg@gmail.com>
2020-10-02 16:42:13 -0500

    commit on master

--- DATA_RECIEVER/RECIEVER.py
+++ DATA_RECIEVER/RECIEVER.py
@@ -1,19 +1,24 @@
 from pylab import *
 from rtlsdr import *
 
-sdr = RtlSdr()
-
-# configure device
-sdr.sample_rate = 2.4e6
-sdr.center_freq = 103.9e6
-sdr.gain = 4
-
-samples = sdr.read_samples(256*1024)
-sdr.close()
-
-# use matplotlib to estimate and plot the PSD
-psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
-xlabel('Frequency (MHz)')
-ylabel('Relative power (dB)')
-
-show()
+class RECEIVER():
+    
+    def __init__(self):
+        
+        self.sdr= RtlSdr()
+        self.sdr.gain=4
+        self.sdr.sample_rate=2.4e6
+        min=input("Enter the min frequency")
+        max=input("Enter the max frequency")
+        
+        for center in range(min,max):
+            self.data_collector(center)
+            center=center+(1e3)
+        
+    def data_collector(self,center):
+        self.sdr.center_freq(center)
+        samples=self.sdr.read_samples(256*1024)
+        
+        
+if __name__ =="__main__":
+    RECEIVER()
