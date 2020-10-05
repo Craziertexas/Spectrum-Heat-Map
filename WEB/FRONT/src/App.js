09acbea2f804f39386b203bc67a2a4738bfeb8ff
camilosalas99 <camiloandress@uninorte.edu.com>
2020-10-05 12:19:19 -0500

    front

--- WEB/FRONT/src/App.js
+++ WEB/FRONT/src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {GoogleMap,LoadScript,Marker,InfoWindow} from "@react-google-maps/api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY= process.env.REACT_APP_SPECTRUM_HEAT_MAP_KEY;
+
+const mapContainerStyle={
+  width: "100vw",
+  height: "100vh",
+};
+
+const center ={
+lat: 4.570868,
+lng: -74.297333,
+};
+
+class App extends Component {
+  //CONSTRUCTOR
+  constructor(){
+    super();
+  }
+  //INICIAL
+  componentDidMount(){
+
+  }
+
+  //LOGICA
+
+  // HTML GUI
+  render() {return(
+
+  <div>
+    <LoadScript
+      googleMapsApiKey={API_KEY}>
+      <GoogleMap 
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+      >
+
+      </GoogleMap>
+    </LoadScript>
+  </div>
+    
+    );
+  }
+  
 }
 
 export default App;
