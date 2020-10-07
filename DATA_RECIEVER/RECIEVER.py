c3a5f189b422ed006f8d005fb0ddc6f024f49a41
David Ruiz <druizeg@gmail.com>
2020-10-07 15:45:09 -0500

    COMMIT ON MASTER

--- DATA_RECIEVER/RECIEVER.py
+++ DATA_RECIEVER/RECIEVER.py
@@ -1,48 +1,55 @@
 from pylab import *
 from rtlsdr import *
 from time import sleep
-import asyncio
+import numpy as np
 
 class RECEIVER():
     
     def __init__(self):
+        self.manager()
+       
         
-        self.loop=asyncio.get_event_loop()
-        self.loop.create_task(self.manager())
-        self.loop.run_forever()
+    def manager(self):
         
-        for i in range(0,100,1):
-            sleep(1)
-            print("Zzzzz")
-        
-    async def manager(self):
-        
+        self.sdr= RtlSdr()
+        self.sdr.gain=0
+        self.sdr.sample_rate=2.4e6
+
         try:
             min=int(input("Enter the min frequency"))
             max=int(input("Enter the max frequency"))
         except:
             print("Wrong numbers, taking defaul values")
-            min=int(100e6)
-            max=int(200e6)
-             
-        self.sdr= RtlSdr()
-        self.sdr.gain=4
-        self.sdr.sample_rate=2.4e6
-        
-        for center in range(min,max,(int(10e6))):
-            buff=await self.data_collector(center)
-            psd(buff, NFFT=1024, Fs=self.sdr.sample_rate/1e6, Fc=self.sdr.center_freq/1e6)
-            xlabel('Frequency (MHz)')
-            ylabel('Relative power (dB)')
-            show()
-        
+            min=int(102e6)
+            max=int(104e6)
+            
+        sw=True
+        number_windows=0
+
+        for center in range(min,max,(int(3e6))):
+            if sw:
+                print("Center: ",center)
+                buff=self.data_collector(center)
+                data=buff
+                sw=False
+            else:
+                print("Center: ",center)
+                buff=self.data_collector(center)
+                data=np.concatenate((data,buff))
+            number_windows=number_windows+1
+
+        s=np.fft.fft(data)
+        f=np.fft.fftfreq(n=(np.shape(data))[0],d=1/self.sdr.sample_rate)
+        plot(f,abs(s))
+        xlabel('Frequency')
+        ylabel('Relative power (dB)')
+        show()
+
         self.sdr.close()
-        self.loop.stop()
         
-    async def data_collector(self,center):
-        
+    def data_collector(self,center):
         self.sdr.center_freq=center
-        samples=self.sdr.read_samples(256*1024)
+        samples=self.sdr.read_samples(1024*1.5)
         return samples
         
 if __name__ =="__main__":
