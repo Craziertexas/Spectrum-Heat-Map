78842615f0096982cb16af0db315be7ef49769c9
David Ruiz <druizeg@gmail.com>
2020-10-07 17:02:59 -0500

    commit on master

--- Scratch/App.js
+++ Scratch/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import {GoogleMap,LoadScript} from "@react-google-maps/api";
-import usePlacesAutocomplete,{getGeocode, getLatLng} from "use-places-autocomplete";
-import{Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption} from "@reach/combobox";
+import PlacesAutocomplete, {geocodeByAddress,getLatLng,} from 'react-places-autocomplete';
 
-const API_KEY=process.env.REACT_APP_MAPS_API;
+const API_KEY_MAPS=process.env.REACT_APP_MAPS_API;
+const API_KEY_PLACES=process.env.REACT_APP_PLACES;
+
 const mapContainerStyle={
   width: "100vw",
   height: "100vh",
@@ -13,19 +14,22 @@ const center ={
 lat: 4.570868,
 lng: -74.297333,
 };
-const ir = React.useCallback(({lat, lng})=>{mapRef.current.ir({lat, lng}); mapRef.current.setZoom(14);}, []);
-<Search ir={ir}/>
 
 class App extends Component {
 
   //CONSTRUCTOR
   constructor(){
     super();
+    this.state={
+      address:'zz'
+    }
   }
 
   //INICIAL
   componentDidMount(){
-  
+    console.log("XXXXXXXXX");
+    console.log(this.state.address);
+    console.log("XXXXXXXXX")
   }
 
   //LOGICA
@@ -34,8 +38,50 @@ class App extends Component {
   render() {return(
 
   <div>
+
+  <div style={{zIndex:'10',position:'absolute',top:'5%',left:'35%',width:'30%'}}>
+  <PlacesAutocomplete
+        value={this.state.address}
+        onChange={address => {this.setState({ address:address });}}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+  </div>
+
+  <div style={{zIndex:'5',position:'absolute'}}>
     <LoadScript
-      googleMapsApiKey={API_KEY}>
+      googleMapsApiKey={API_KEY_MAPS}>
 
       <GoogleMap 
         mapContainerStyle={mapContainerStyle}
@@ -47,48 +93,11 @@ class App extends Component {
 
     </LoadScript>
   </div>
-    
+
+  </div>
     );
   }
   
 }
 
-
-function Search({ir}){
-const {ready, value, suggestions: {status,data}, setvalue, clearSuggestions} = usePlacesAutocomplete({
- requestOptions:{
-   location: {lat:()=> 4.570868, lng: ()=> -74.297333}
-   radius:200*1000;
- }
-
-});
-
-return (
-<Combobox> onSelect={ async (address) =>{
-  onSelect={(address) =>{
-      setvalue(address,false);
-      clearSuggestions();
-      console.log(address);
-  
-  try {
-      const results = await getGeocode({address});
-      const {lat, lng} = await getLatLng(results[0]);
-      console.log(lat,lng);
-      ir({lat, lng});
-  }
-  catch(error){console.log("error!")};
-  //console.log(address);
-  }}
-<ComboboxInput value={value} onChange={(e)=>{
-  setvalue(e.target.value)
-}}
-  disabled={!ready}
-  placeholder="Ingrese una Localidad"
-/>
-
-<ComboboxPopover>
-  {status==="OK" && data.map(({id, description})=>( <ComboboxOption key={id} value= {description}/> ))}
-</ComboboxPopover>
-</Combobox>
-)}
 export default App;
--- WEB/FRONT/src/App.js
+++ WEB/FRONT/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {GoogleMap,LoadScript,Marker,InfoWindow} from "@react-google-maps/api";
+import {GoogleMap,LoadScript} from "@react-google-maps/api";
+import PlacesAutocomplete, {geocodeByAddress,getLatLng,} from 'react-places-autocomplete';
 
-const API_KEY= process.env.REACT_APP_SPECTRUM_HEAT_MAP_KEY;
+const API_KEY_MAPS=process.env.REACT_APP_MAPS_API;
+const API_KEY_PLACES=process.env.REACT_APP_PLACES;
 
 const mapContainerStyle={
   width: "100vw",
@@ -16,13 +16,20 @@ lng: -74.297333,
 };
 
 class App extends Component {
+
   //CONSTRUCTOR
   constructor(){
     super();
+    this.state={
+      address:'zz'
+    }
   }
+
   //INICIAL
   componentDidMount(){
-
+    console.log("XXXXXXXXX");
+    console.log(this.state.address);
+    console.log("XXXXXXXXX")
   }
 
   //LOGICA
@@ -31,8 +38,51 @@ class App extends Component {
   render() {return(
 
   <div>
+
+  <div style={{zIndex:'10',position:'absolute',top:'5%',left:'35%',width:'30%'}}>
+  <PlacesAutocomplete
+        value={this.state.address}
+        onChange={address => {this.setState({ address:address });}}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+  </div>
+
+  <div style={{zIndex:'5',position:'absolute'}}>
     <LoadScript
-      googleMapsApiKey={API_KEY}>
+      googleMapsApiKey={API_KEY_MAPS}>
+
       <GoogleMap 
         mapContainerStyle={mapContainerStyle}
         zoom={8}
@@ -40,9 +90,11 @@ class App extends Component {
       >
 
       </GoogleMap>
+
     </LoadScript>
   </div>
-    
+
+  </div>
     );
   }
   
