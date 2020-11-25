6ac8cdc0685a2b3146548222dbb1b15c1b16f0c7
David Ruiz <druizeg@gmail.com>
2020-11-25 04:35:40 -0500

    Frequency_analizer (done), Back (still), MySQL_Tables (next)

--- DATA_RECIEVER/Frequency_Analizer.py
+++ DATA_RECIEVER/Frequency_Analizer.py
@@ -1,85 +1,121 @@
 from rtlsdr import *
-import time as TimeModule
+from scipy import signal
+from math import log10
+import json
 import numpy as np
 import matplotlib.pyplot as plt 
+import socket
+import requests
 
 class FreqAnalizer():
 
     def __init__(self):
-        self.SdrSetup()
-        self.FreqRangeInput()
-        mag, freq = self.SampleLoop()
-        mag, freq = self.Data_processing(mag,freq)
-        plt.plot(freq, 20*np.log10(mag))
-        plt.show()
-        #CellPhone GPS
-        #Send to DataBase
+        #SDR Interface
+        self.SDR = RtlSdr()
+        self.SDR.set_bandwidth = 3e6
+        self.SDR.set_sample_rate = 3e6
+        self.SDR.set_gain = 0
+        #UDP Server
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind(('192.168.20.64', 40000))
+        self.mainloop()
 
-    def SdrSetup(self):
-        self.sdr = RtlSdr()
-        self.sdr.gain = 4
-        self.sdr.sample_rate = 2.4e6 #Sample Rate >= 2*Max Readable Freq
-    
-    def FreqRangeInput(self):
-        try:
-            self.min_freq = int(input("Set min Frequency"))
-            self.max_freq = int(input("Set max Frequency"))
-            print("Min: {} Hz",self.min_freq)
-            print("Max: {} Hz",self.max_freq)
-        except:
-            print("Wrong input, taking default settings")
-            self.min_freq = 100e6
-            self.max_freq = 170e6
-            print("Min: {} Hz",self.min_freq)
-            print("Max: {} Hz",self.max_freq)
-    
-    def SampleLoop(self):
-        sw = True
-        for center in range(int(self.min_freq),int(self.max_freq),int(2e6)):
-            if sw:
-                print("Center: {} Hz",center)
-                buff = self.DataCollector(center)
-                mag, freq = self.FFT_Generator(buff)
-                sw = False
-            else:
-                print("Center: {} Hz",center)
-                buff = self.DataCollector(center)
-                buffmag, bufffreq = self.FFT_Generator(buff)
-                mag = np.concatenate((mag, buffmag))
-                freq = np.concatenate((freq, bufffreq))
-        return mag,freq
-    
-    def DataCollector(self, center):
-        self.sdr.center_freq = center
-        samples = self.sdr.read_samples(256*1024)
-        return samples
+    def mainloop(self):
+        while True:
+            capture_sw = bool(input('Tomar datos?'))
+            if capture_sw:
+                Freqs, Power = self.get_spectrum(min_freq= 100, max_freq= 1700, numsamples= 256, avgsamples=1)
+                Freqs, Power = self.formmater(power=Power, freq=Freqs)
+                plt.plot(Freqs, Power)
+                plt.show()
+                data_sw = bool(input('Espectro correcto?'))
+                if data_sw:
+                    print('Esperando coordenadas...')
+                    coordinates = self.gps()
+                    packet = {"power": Power.tolist(),"frequencies":Freqs.tolist() ,"coordinates":{"lng":coordinates[0],"lat":coordinates[1]}}
+            http_sw = True
+            while http_sw:
+                response = requests.post("http://localhost:4000",json.dumps(packet,indent=4))
+                print('Paquete:')
+                print(packet)
+                print(response)
+                http_sw = bool(input('Se envió correctamente?'))
 
-    def FFT_Generator(self,data):
-        psd = plt.psd(data, Fs=(self.sdr.sample_rate/1e6), Fc=(self.sdr.center_freq/1e6),NFFT=1024)
-        mag = psd[0]
-        freq = psd[1]
-        return mag, freq
-    
-    def Data_processing(self, mag, freq):
-        medium = int(((mag.size) /2))
-        mag[medium - 1] = mag[medium - 2]
-        mag[medium] = mag[medium - 1]
-        mag[medium + 1] = mag[medium]
-        sw = True
-        buffmag = np.empty(freq.size)
-        bufffreq = np.empty(freq.size)
-        for i in range(0,(freq.size -1)):
-            if sw:
-                z = 0
-                buffmag[z] = mag[i]
-                bufffreq[z] = freq[i]
-                sw = False
-            else:
-                if freq[i] != freq[i - 1]:
-                    z = z +1
-                    bufffreq[z] = freq[i]
-                    buffmag[z] = mag[i]
-        return buffmag, bufffreq
+
+    def formmater(self, power, freq):
+        Power_np = np.array(power)
+        Freqs_np = (np.array(freq)).astype(int)
+        Freq_base = 99
+        Power_new = np.zeros(1600)
+        Freqs_new = np.zeros(1600)
+        lastz = 0
+        for i in range (0,1600):
+            Freq_base = Freq_base + 1
+            print('Looking for {}'.format(Freq_base))
+            zwrite = True
+            n = 0
+            for z in range(0, np.shape(Freqs_np)[0]):
+                if zwrite:
+                    z = lastz
+                    zwrite = False
+                if Freqs_np[z] == Freq_base:
+                    print('FOUND!')
+                    Power_new[i] = Power_np[z] + Power_new[i]
+                    n = n + 1
+                    lastz = z
+            Power_new[i] = Power_new[i]/n
+            Freqs_new[i] = Freq_base
+            if Freq_base > 1700:
+                break
+        Power_new = Power_new.astype(int)
+        print(Power_new)
+        print(Freqs_new)
+        return Freqs_new, Power_new
+        
+
+    def gps(self):
+        sw_coordinates = False
+        while not sw_coordinates:
+            data, address = self.socket.recvfrom(4096)
+            print('recieved {} from {}'.format(len(data), address))
+            data = data.decode()
+            data = data.split('\n')
+            print('Data:')
+            print(data)
+            sw_coordinates = bool(input('Coordenadas correctas?'))
+        if sw_coordinates:
+            return data
+
+    #https://onceinawhileprojects.wordpress.com/2018/05/27/sdr-spectrum-analyzer-python/
+    #Metodo tomado de https://bitbucket.org/LightEmittingDude/sdr-speca/src/master/
+    def get_spectrum(self, min_freq, max_freq, numsamples, avgsamples):
+        """function that gets called repeatedly to get the spectrum"""
+        xData = []
+        yData = []
+        trimRatio = 0.75 # this is the ratio of the FFT bins taken to remove FFT edge effects
+        # read samples that covers the required frequency span
+        #User Output
+        print("Frequency Bandwidth min: {}, max: {}".format(min_freq, max_freq))
+        print("Initializing...")
+        self.SDR.center_freq = min_freq * 1e6 + (self.SDR.set_sample_rate * trimRatio) / 2
+        while self.SDR.center_freq < (max_freq * 1e6 + (self.SDR.set_sample_rate * trimRatio) / 2):
+            #User Output
+            print("Center Frequency: {}".format(self.SDR.center_freq))
+            # read samples from SDR
+            samples = self.SDR.read_samples(avgsamples*numsamples)
+            # calculate power spectral density
+            f, pxx = signal.welch(samples, fs=self.SDR.set_sample_rate, nperseg=numsamples)
+            # rotate the arrays so the plot values are continuous and also trim the edges
+            f = list(f)
+            pxx = list(pxx)
+            f = f[int(numsamples/2 + numsamples*(1-trimRatio)/2):] + f[:int(numsamples/2 - numsamples*(1-trimRatio)/2)]
+            pxx = pxx[int(numsamples/2 + numsamples*(1-trimRatio)/2):] + pxx[:int(numsamples/2 - numsamples*(1-trimRatio)/2)]
+            # adjust the format of the values to be plotted and add to plot arrays
+            xData = xData + [(x+self.SDR.center_freq)/1e6 for x in f]
+            yData = yData + [10*log10(y) for y in pxx]
+            # calculate the next center frequency
+            self.SDR.center_freq = self.SDR.center_freq + (self.SDR.set_sample_rate * trimRatio)
+        return xData, yData
 
 if __name__ == "__main__":
     FreqAnalizer()
--- WEB/API/routes/index.js
+++ WEB/API/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var udp = require('dgram');
+
+const server = udp.createSocket('udp4');
+server.bind(50000);
+server.on('listening', function(){
+  console.log('Server Listening');
+});
+server.on('message', function(msg){
+  console.log('Message!');
+  console.log(msg);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req,res,next) {
+  console.log('DATA POST');
+  console.log(req.body.power);
+  res.json('Ok');
 });
 
 module.exports = router;
